3f840c94015d1bd1c521bad129e012c4ab4e90ad
bed-deb <julus06@o2.pl>
2025-05-06 12:00:19 +0200

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -3,18 +3,9 @@ document.addEventListener('gesturestart', function (e) {
   e.preventDefault();
 });
 
+// Pozostawiamy tylko to, co konieczne:
 document.addEventListener('touchmove', function (e) {
   if (e.scale !== 1) {
     e.preventDefault();
   }
 }, { passive: false });
-
-// Blokowanie double-tap zoom
-let lastTouchEnd = 0;
-document.addEventListener('touchend', function (e) {
-  const now = new Date().getTime();
-  if (now - lastTouchEnd <= 300) {
-    e.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
